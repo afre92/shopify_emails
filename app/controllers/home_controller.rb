d170f7248edbb45f53056aac52e0df1768d30e5b
Andres <afre92@gmail.com>
2019-12-22 21:23:37 -0500

    main route changes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class HomeController < AuthenticatedController
-  def index
+  def show
     @products = ShopifyAPI::Product.find(:all, params: { limit: 10 })
     @webhooks = ShopifyAPI::Webhook.find(:all)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'template', to: 'templates#index'
-  root :to => 'home#index'
+  get 'template/edit', to: 'templates#edit'
+  root :to => 'home#show'
   mount ShopifyApp::Engine, at: '/'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
