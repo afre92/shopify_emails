b41c0e436e43cfd9165edfd412bf9aae2e08dd52
Andres <afre92@gmail.com>
2019-12-22 20:31:51 -0500

    shopify controllers generated, templte view working

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,7 +1,12 @@
 class Shop < ActiveRecord::Base
   include ShopifyApp::SessionStorage
+  after_create :add_email
 
   def api_version
     ShopifyApp.configuration.api_version
+  end
+
+  def add_email
+  	byebug
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'templates', to: 'templates#index'
+  get 'template', to: 'templates#index'
   root :to => 'home#index'
   mount ShopifyApp::Engine, at: '/'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
