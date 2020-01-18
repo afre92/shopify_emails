6b6d5b0c32b29d5421348589cf916d63815cbe0e
Andres <afre92@gmail.com>
2020-01-17 20:59:56 -0500

    bug on getting shop info fixed

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,10 +1,10 @@
 class Shop < ActiveRecord::Base
   include ShopifyApp::SessionStorage
-  has_one :thank_you_template, dependent: :destroy
+  has_one :thank_you_template, dependent: :delete
   after_create :get_shop_info, :associate_templates 
 
   def associate_templates
-    ThankYouTemplate.create(shop_id: self.id)
+    ThankYouTemplate.create(shop_id: self.id, from: self.email, reply_to: self.email)
   end
 
   def api_version
@@ -19,7 +19,11 @@ class Shop < ActiveRecord::Base
 
     self.email = shop_info['email']
     self.address = shop_info['address1']
-    self.owner_first_name = shop_info['shop_owner']
+    if shop_info['shop_owner'].include?(" ") 
+      self.owner_first_name, self.owner_last_name = shop_info['shop_owner'].split(" ")
+    else
+      self.owner_first_name = shop_info['shop_owner']
+    end
     self.phone = shop_info['phone']
     self.shop_name = shop_info['name']
     self.save
