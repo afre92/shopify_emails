cf3699d527156584dfd9bff49de4008689587517
Andres <afre92@gmail.com>
2020-01-23 21:36:30 -0500

    layout imported to template controller view

--- app/mailers/customer_mailer.rb
+++ app/mailers/customer_mailer.rb
@@ -3,14 +3,14 @@ class CustomerMailer < ApplicationMailer
 
   def send_thank_you_email#(customer)
     @shop = Shop.last
-    @template = @shop.thank_you_template
+    @template = @shop.templates.first
     mail( :to => 'afre92@gmail.com',
     :subject => 'Thank for your purchase' )
   end
 
   def send_please_review_email#(user)
     @shop = Shop.last
-    @template = @shop.thank_you_template
+    @template = @shop.templates.first
     mail( :to => 'afre92@gmail.com',
     :subject => 'Please review the items you recieved' )
   end
