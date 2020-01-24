a35ef6754813147e7fae941c6ac9a0b38dabc99e
Andres <afre92@gmail.com>
2020-01-23 21:46:44 -0500

    email model added

---
+++ db/migrate/20200124024548_create_emails.rb
@@ -0,0 +1,14 @@
+class CreateEmails < ActiveRecord::Migration[5.2]
+  def change
+    create_table :emails do |t|
+      t.datetime :scheduled_time
+      t.integer :was_sent, default: 0
+      t.integer :tracking_pixel_id
+      t.integer :template_id
+      t.integer :shop_id
+      t.integer :order_id
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_24_013720) do
+ActiveRecord::Schema.define(version: 2020_01_24_024548) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "emails", force: :cascade do |t|
+    t.datetime "scheduled_time"
+    t.integer "was_sent", default: 0
+    t.integer "tracking_pixel_id"
+    t.integer "template_id"
+    t.integer "shop_id"
+    t.integer "order_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "order_items", force: :cascade do |t|
     t.string "shopify_id"
