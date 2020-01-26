01c39144b680b5b45b54d598ce77e44299dc139c
Andres <afre92@gmail.com>
2020-01-25 22:12:09 -0500

    new styls and before action to settings

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -6,12 +6,17 @@ class SettingsController < ApplicationController
   end
 
   def update
-  byebug
+    @shop.update(shop_params)
   end
 
   private
+
     def find_store
       @shop = Shop.find_by(shopify_domain: session[:shopify_domain])
+    end
+
+    def shop_params
+      params.require(:shop).permit(:status, :thank_you_interval, :review_interval, :shop_name, :title, :owner_first_name, :owner_last_name, :phone, :address, :email, :website)
     end
 
 end
