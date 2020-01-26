59a866fd573eb0555a8f62759df5035df4c50649
Andres <afre92@gmail.com>
2020-01-25 23:10:33 -0500

    Dashboard contrller and views create to host dashboard view

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   get '/settings', to: 'settings#edit'
   patch '/settings', to: 'settings#update'
   resources :templates
-  root :to => 'home#show'
+  # get '/dashboard', to: 'dashboard#index'
+  root :to => 'dashboard#index'
   mount ShopifyApp::Engine, at: '/'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
