5ea335476216f6e9f39579b2c2466387842fbe7d
Andres <afre92@gmail.com>
2020-01-31 17:05:03 -0500

    wip

--- app/jobs/order_create_job.rb
+++ app/jobs/order_create_job.rb
@@ -2,6 +2,9 @@ class OrderCreateJob < ActiveJob::Base
   def perform(shop_domain:, webhook:)
     shop = Shop.find_by(shopify_domain: shop_domain)
 
+    return unless shop.status == 1
+
+    # Create Order for Thank You Email
     new_order = Order.new
     new_order.shopify_id = webhook['id'].to_s
     new_order.email = webhook['email']
@@ -10,27 +13,36 @@ class OrderCreateJob < ActiveJob::Base
     new_order.customer = webhook['customer'].to_json
     new_order.save
 
-    webhook['line_items'].each do |item|
-      new_order_item = OrderItem.new
-      new_order_item.shopify_id = item['id'].to_s
-      new_order_item.variant_id = item['variant_id'].to_s
-      new_order_item.title = item['title']
-      new_order_item.quantity = item['quantity']
-      new_order_item.order_id = new_order.id
-      new_order_item.save
+    # Create Thank You Email
+    thank_you_email = Email.new 
+    thank_you_email.scheduled_time = new_order.shopify_created_at + shop.thank_you_interval.minutes
+    thank_you_email.template_id = shop.templates.find_by(template_type: 0).id
+    thank_you_email.shop_id = shop.id
+    thank_you_email.order_id = new_order.id
+    thank_you_email.save
+
+    # Create Order Items and Review Email if subscription_type == 1
+    if shop.subscription_type == 1
+
+      webhook['line_items'].each do |item|
+        new_order_item = OrderItem.new
+        new_order_item.shopify_id = item['id'].to_s
+        new_order_item.variant_id = item['variant_id'].to_s
+        new_order_item.title = item['title']
+        new_order_item.quantity = item['quantity']
+        new_order_item.order_id = new_order.id
+        new_order_item.save
+      end
+
+      # Create Review Email
+      review_email = Email.new 
+      review_email.scheduled_time = new_order.shopify_created_at + shop.review_interval.minutes
+      review_email.template_id = shop.templates.find_by(template_type: 1).id
+      review_email.shop_id = shop.id
+      review_email.order_id = new_order.id
+      review_email.save
+
     end
-
-    
-    # create and schedule email to be sent
-    #thank you email
-    # shop.templates.find_by(template_type: 0)
-
-    new_email = Email.new 
-    new_email.scheduled_time = new_order.shopify_created_at + 30.minutes
-    new_email.template_id = shop.templates.find_by(template_type: 0).id
-    new_email.shop_id = shop.id
-    new_email.order_id = new_order.id
-    new_email.save
 
 
     shop.with_shopify_session do
