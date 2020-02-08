73a2489ac374ce12fc540dc4513d907df938e12b
Andres <afre92@gmail.com>
2020-02-08 18:28:54 -0500

    format of customer obj changed to be access via dot notation, rename function to send email

--- app/mailers/customer_mailer.rb
+++ app/mailers/customer_mailer.rb
@@ -1,22 +1,14 @@
 class CustomerMailer < ApplicationMailer
-  # default :from => 'your_store@email.com'
 
-  #both email will use this method, maybe change name  
-  def send_thank_you_email(email)
+  def send_email(email)
     @email = email
     @shop = Shop.find(@email.shop_id)
-    @customer = JSON.parse(@shop.orders.find(@email.order_id).customer)
+    @customer = JSON.parse(@shop.orders.find(@email.order_id).customer, object_class: OpenStruct)
     @template = @shop.templates.find(@email.template_id)
 
-    mail( to: @customer["email"],
+    mail( to: @customer.email,
     subject: @template.subject,
     from: @template.from)
   end
 
-  # def send_please_review_email#(user)
-  #   @shop = Shop.last
-  #   @template = @shop.templates.first
-  #   mail( :to => 'afre92@gmail.com',
-  #   :subject => 'Please review the items you recieved' )
-  # end
 end
