ae870899303ae88de40ec6d5be9c2a98f03b0723
Andres <afre92@gmail.com>
2020-02-09 17:12:55 -0500

    graph data working

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,16 +4,24 @@ class DashboardController < ApplicationController
   before_action :find_shop
 
   def index
-    # @daterange = # ? params[:daterange] : ""
     @daterange = params[:daterange] ? format_daterange(params[:daterange]) : (DateTime.now.beginning_of_month...DateTime.now)
     @emails_sent = @shop.emails.sent.where(created_at: @daterange).count
     @emails_used = @shop.emails.count
     @emails_opened = @shop.emails.where(created_at: @daterange).sent.opened
+    email_graph_data
   end
 
   def format_daterange(daterange)
     from, to = daterange.split('-')
     (DateTime.strptime(from.remove(' '), '%m/%d/%Y')...DateTime.strptime(to.remove(' '), '%m/%d/%Y'))
+  end
+
+  def email_graph_data
+    @graph_data = {}
+    @daterange.each do |date|
+      @graph_data[date.strftime('%m/%d/%Y')] = [@shop.emails.sent.where(created_at: date...date.end_of_day).count, @shop.emails.where(created_at: date...date.end_of_day).sent.opened]
+    end
+    return @graph_data
   end
 
   private
