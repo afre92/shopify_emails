4aa2604b5f49c27d6322a8d53bd79cd61bb0f6b5
Andres <afre92@gmail.com>
2020-02-22 13:08:56 -0500

    billing plan confirmation rendering

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -9,6 +9,33 @@ class SettingsController < AuthenticatedController
     @shop.update(shop_params)
   end
 
+  def create_recurring_application_charge
+    unless ShopifyAPI::RecurringApplicationCharge.current
+      recurring_application_charge = ShopifyAPI::RecurringApplicationCharge.new(
+              name: "Basic plan",
+              price: 9.99,
+              return_url: "#{ENV['APP_URL']}activatecharge",
+              test: true,
+              trial_days: 7,
+              capped_amount: 100,
+              terms: "$0.99 for every order created")
+   
+      if recurring_application_charge.save
+        # @tokens[:confirmation_url] = recurring_application_charge.confirmation_url
+        # byebug
+      @url = recurring_application_charge.confirmation_url
+      end
+    end
+   end
+  
+  def activate_charge
+    byebug
+    recurring_application_charge = ShopifyAPI::RecurringApplicationCharge.find(request.params['charge_id'])
+    recurring_application_charge.status == "accepted" ? recurring_application_charge.activate : redirect_to('https://test-email-thing.myshopify.com/admin/apps')
+
+    redirect_to 'https://test-email-thing.myshopify.com/admin/apps'
+  end
+
   private
 
   def find_store
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,10 @@ Rails.application.routes.draw do
   get '/settings', to: 'settings#edit', as: 'settings'
   patch '/settings', to: 'settings#update'
   resources :templates
-  get '/convert/:token' => 'analytics#convert', as: :views
+  get '/convert/:token', to: 'analytics#convert', as: :views
   root to: 'home#index'
+  get 'activatecharge', to: 'settings#activate_charge'
+  get 'create-charge', to: 'settings#create_recurring_application_charge', as: 'create_charge'
   mount ShopifyApp::Engine, at: '/'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
