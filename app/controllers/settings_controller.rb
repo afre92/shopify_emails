03053261336e58738dcb43ee471dda58e92d3469
Andres <afre92@gmail.com>
2020-02-22 18:02:12 -0500

    redirecting to setting page after subscription upgrade

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -3,33 +3,31 @@
 class SettingsController < AuthenticatedController
   before_action :find_store
 
-  def edit; end
+  def edit 
+
+  end
 
   def update
     @shop.update(shop_params)
   end
 
-  def remove_plan
-
-  end
-
   def cancel_charge
-    # check for the presence of current
-    if ShopifyAPI::RecurringApplicationCharge.current.cancel
-      #change subscription_type
-    end
+    ShopifyAPI::RecurringApplicationCharge.current.cancel
+    @shop.update(subscription_type: 0)
+    redirect_to settings_path
   end
 
   def create_recurring_application_charge
     unless ShopifyAPI::RecurringApplicationCharge.current
       recurring_application_charge = ShopifyAPI::RecurringApplicationCharge.new(
-              name: "Basic plan",
-              price: 9.99,
-              return_url: "#{ENV['APP_URL']}/activatecharge",
-              test: true,
-              trial_days: 7,
-              capped_amount: 100,
-              terms: "$0.99 for every order created")
+        name: "Basic plan",
+        price: 9.99,
+        return_url: "#{ENV['APP_URL']}/activatecharge",
+        test: true,
+        trial_days: 7,
+        capped_amount: 100,
+        terms: "$0.99 for every order created"
+      )
    
       if recurring_application_charge.save
         @url = recurring_application_charge.confirmation_url
@@ -40,8 +38,7 @@ class SettingsController < AuthenticatedController
    end
   
   def activate_charge
-    # TODO: redirect back original page
-    redirect_link = "https://#{@shop.shopify_domain}/admin/apps"
+    redirect_link = "https://#{@shop.shopify_domain}/admin/apps/#{ENV['APP_NAME']}/settings"
     recurring_application_charge = ShopifyAPI::RecurringApplicationCharge.find(request.params['charge_id'])
     if recurring_application_charge.status == "accepted" 
        recurring_application_charge.activate
