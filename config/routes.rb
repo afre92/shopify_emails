ec6ed3ffde00d1d7b293984398780a475c643d90
Andres <afre92@gmail.com>
2020-05-21 18:25:37 -0400

    bug where graph would not work on home screen fixed

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,8 @@ Rails.application.routes.draw do
 
   get '/emails', to: 'emails#index', as: 'emails'
   get '/email/:id', to: 'emails#show', as: 'email'
+
+  get '/dashboard', to: 'home#index'
   mount ShopifyApp::Engine, at: '/'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
