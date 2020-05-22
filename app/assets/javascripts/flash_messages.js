bc5a3e0dccdde0900d46f1204f2c09aa90ba2d61
Andres <afre92@gmail.com>
2020-05-22 15:46:33 -0400

    js files removed to fix error logs

--- app/assets/javascripts/flash_messages.js
+++ app/assets/javascripts/flash_messages.js
@@ -1,17 +1,19 @@
-var eventName = typeof(Turbolinks) !== 'undefined' ? 'turbolinks:load' : 'DOMContentLoaded';
 
-if (!document.documentElement.hasAttribute("data-turbolinks-preview")) {
-  document.addEventListener(eventName, function flash() {
-    var flashData = JSON.parse(document.getElementById('shopify-app-flash').dataset.flash);
 
-    if (flashData.notice) {
-      ShopifyApp.flashNotice(flashData.notice);
-    }
+// var eventName = typeof(Turbolinks) !== 'undefined' ? 'turbolinks:load' : 'DOMContentLoaded';
 
-    if (flashData.error) {
-      ShopifyApp.flashError(flashData.error);
-    }
+// if (!document.documentElement.hasAttribute("data-turbolinks-preview")) {
+//   document.addEventListener(eventName, function flash() {
+//     var flashData = JSON.parse(document.getElementById('shopify-app-flash').dataset.flash);
 
-    document.removeEventListener(eventName, flash)
-  });
-}
+//     if (flashData.notice) {
+//       ShopifyApp.flashNotice(flashData.notice);
+//     }
+
+//     if (flashData.error) {
+//       ShopifyApp.flashError(flashData.error);
+//     }
+
+//     document.removeEventListener(eventName, flash)
+//   });
+// }
