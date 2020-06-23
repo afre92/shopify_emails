3021d5f1dc5dfe704634e80034890de7c63c6e1d
Andres <afre92@gmail.com>
2020-06-23 14:24:14 -0400

    extra migration added

--- db/migrate/20200415155654_add_order_item_to_reviews.rb
+++ db/migrate/20200415155654_add_order_item_to_reviews.rb
@@ -1,5 +1,6 @@
 class AddOrderItemToReviews < ActiveRecord::Migration[5.2]
   def change
+    add_column :reviews, :exported, :boolean, default: false
     add_reference :reviews, :order_item, foreign_key: true
     add_column :reviews, :uuid, :string, default: ''
   end
--- db/migrate/20200502003320_add_exported_to_reviews.rb
+++
@@ -1,5 +0,0 @@
-class AddExportedToReviews < ActiveRecord::Migration[5.2]
-  def change
-    add_column :reviews, :exported, :boolean, default: false
-  end
-end
