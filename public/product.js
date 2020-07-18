3fc35ca27846f32ba68f05f8427a18db1fa4ef15
Andres <afre92@gmail.com>
2020-07-18 17:14:35 -0400

    reviews showing correctly on store front

--- public/product.js
+++ public/product.js
@@ -2,29 +2,7 @@ function loadReaviews() {
   var shopifyId = $("#shopify-product-reviews").data('id');
   $.get( `${window.location.origin}/a/s`, { id: shopifyId })
     .done(function(data){
-      
-      console.log('dara recieved')
-      // data.replace(/&quot;/g, '"').replace(/&ldquo;/g, '"').replace(/&rdquo;/g, '"').replace(/lsquo;/g, '"').replace(/&rsquo;/g, '"').replace(/&lt;/g, '<').replace(/&gt;/g, '>')
-      debugger
-      var list = data.map((review) => {
-      `<div class="review" style="border-bottom: 1px black solid;">
-              <div>
-                number of stars here
-              </div>
-              <div style="padding-top:10px; padding-bottom: 10px;">
-                <div>
-                  <b>Title </b>
-                </div>
-                <div>
-                  Andres Rojas
-                </div>
-              </div>
-              <div style="padding-bottom: 20px;">
-                and the description goes here
-              </div>
-            </div>`})
-            debugger
-            
+      $('#reviews-list').html(data)
     })
     .fail(function(){
       console.log("something failed")
